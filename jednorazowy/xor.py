5184dd5d7be1bd39161f81ff4a3aa1b7460a6ffb
milankwiatkowski <milan.mikolaj.kwiatkowski@gmail.com>
2025-04-05 11:45:58 +0200

    deecrypted almost

--- jednorazowy/xor.py
+++ jednorazowy/xor.py
@@ -48,4 +48,43 @@ elif args.szyfr:
     crypto = os.open("crypto.txt", os.O_WRONLY | os.O_CREAT, 0o644)
     for elems in tab:
         os.write(crypto, (str(elems) + "\n").encode("utf-8"))
-# elif args.crypto:
+elif args.crypto:
+    with open("crypto.txt") as crypto:
+        zaszyfrowane = [line.rstrip("\n") for line in crypto]
+    bajty_int=[]
+    for i in range(len(zaszyfrowane)):
+        zaszyfrowane[i] = zaszyfrowane[i].split("/")
+        zaszyfrowane[i] = [b for b in zaszyfrowane[i] if b]
+        bajty_int.append([int(b, 16) >> 13 for b in zaszyfrowane[i]])
+    decrypt = [["_" for _ in range(64)] for _ in range(len(zaszyfrowane))]
+    podejrzenia=[]
+    for i in range(len(zaszyfrowane)):
+        linia = []
+        for k in range(len(zaszyfrowane[i])):
+            linia.append({})
+        podejrzenia.append(linia)
+    for i in range(len(bajty_int)):
+        for j in range(len(bajty_int)):
+            if i != j:
+                xor_linia = [x ^ y for x, y in zip(bajty_int[i], bajty_int[j])]
+                for k in range(len(xor_linia)):
+                    if (xor_linia[k] & 0b11100000 == 0b01000000):
+                        litera = bajty_int[i][k] ^ 32
+                        if (96 < litera < 123):
+                            if podejrzenia[i][k].get(chr(litera)):
+                                podejrzenia[i][k][chr(litera)] += 1
+                            else:
+                                podejrzenia[i][k][chr(litera)] = 1
+                        else:
+                            litera = bajty_int[j][k] ^ 32
+                            if (96 < litera < 123):
+                                if podejrzenia[i][k].get(chr(litera)):
+                                    podejrzenia[i][k][chr(litera)] += 1
+                                else:
+                                    podejrzenia[i][k][chr(litera)] = 1
+    for i in range(len(podejrzenia)):
+        print(podejrzenia[i])
+        # for (litera, liczba) in podejrzenia[i].items():
+        #     faworyt="_"
+        #     najwiecej=0
+        #     print(litera,liczba)
